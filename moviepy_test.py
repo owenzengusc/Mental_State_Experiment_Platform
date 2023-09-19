4bac1685180fcf189d49ee1f4db57f24c9cae02f
Owen Zeng <owenzeng@usc.edu>
2023-09-19 01:05:39 -0700

    add_moviepy_test.py

--- moviepy_test.py
+++ moviepy_test.py
@@ -2,5 +2,5 @@ from moviepy.editor import *
 #video = VideoFileClip("./videos/sample-mp4-file.mp4")
 video = VideoFileClip("./videos/test.mp4")
 video = video.subclip(0, 10)
-video.resize(2)
+#video.resize(2)
 video.preview(fps = 60,fullscreen = True)
