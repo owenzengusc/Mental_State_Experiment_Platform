00e7937e6d9cd949963666f21d4a41a92e0106c9
Owen Zeng <owenzeng@usc.edu>
2023-09-19 01:14:25 -0700

    check

--- video.py
+++ video.py
@@ -2,10 +2,7 @@ from tkinter import *
 import datetime
 import tkinter
 from tkinter import filedialog
-#pip install tkvideoplayer
-from tkVideoPlayer import TkinterVideo
 import sys
-import pygame
 import os
+from moviepy.editor import *
 sys.path.append('videos')
-pygame.mixer.init()
